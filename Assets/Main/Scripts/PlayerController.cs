1f413baf2b777e41765b4f9cce9dc4799d1cbb73
reaper2112 <olegermolaev54@gmail.com>
2025-02-10 00:00:16 +0300

    added player controller and platform

---
+++ Assets/Main/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController cc;
+    [SerializeField] private GameObject player;
+    [SerializeField] private Camera cam;
+    [SerializeField] private float Sensitivity;
+
+    [SerializeField] private float speed, walk, run;
+
+    private Vector3 normalScale;
+
+    public bool isMoving, isRunning;
+
+    private float X, Y;
+
+    private void Start()
+    {
+        speed = walk;
+        normalScale = new Vector3(1, 1, 1);
+        cc = GetComponent<CharacterController>();
+        cc.enabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    private void Update()
+    {
+        #region Camera Limitation Calculator
+        //Camera limitation variables
+        const float MIN_Y = -60.0f;
+        const float MAX_Y = 70.0f;
+
+        X += Input.GetAxis("Mouse X") * (Sensitivity * Time.deltaTime);
+        Y -= Input.GetAxis("Mouse Y") * (Sensitivity * Time.deltaTime);
+
+        if (Y < MIN_Y)
+            Y = MIN_Y;
+        else if (Y > MAX_Y)
+            Y = MAX_Y;
+        #endregion
+        transform.localRotation = Quaternion.Euler(Y, X, 0.0f);
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 forward = transform.forward * vertical;
+        Vector3 right = transform.right * horizontal;
+
+        cc.SimpleMove((forward + right) * speed);
+        // Determines if the speed = run or walk
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = run;
+            isRunning = true;
+        }
+
+        else
+        {
+            isRunning = false;
+            speed = walk;
+            player.transform.localScale = normalScale;
+        }
+        // Detects if the player is moving.
+        // Useful if you want footstep sounds and or other features in your game.
+        isMoving = cc.velocity.sqrMagnitude > 0.0f ? true : false;
+    }
+}
